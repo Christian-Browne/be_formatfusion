e18b772366be35e8dd86ef025ddb9c7fda911b5a
Christian Browne <christianbrowne33@gmail.com>
2024-04-21 17:40:43 -0700

    url fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://fe-fromatfusion.vercel.app/"
+        "https://fe-fromatfusion.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
